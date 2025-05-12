e1bd96e4ec5bc0a3b3f1da753317fb2fd0c1b817
Kasey Endsley <kaseyendsley@gmail.com>
2025-05-12 14:10:54 -0500

    Added About & Resources

--- components/welcome/About.jsx
+++ components/welcome/About.jsx
@@ -2,7 +2,7 @@ export const About = () => {
     return (
       <div className="home-container">
         <h1 className="home-title">About Bowie's Friends</h1>
-        <p> Words. </p>
+        <p> Words, eventually :) </p>
       </div>
     );
   };
--- components/welcome/Resources.jsx
+++ components/welcome/Resources.jsx
@@ -2,7 +2,7 @@ export const Resources = () => {
     return (
       <div className="home-container">
         <h1 className="home-title">Community Resources</h1>
-        <p> Words. </p>
+        <p> Words, eventually :) </p>
       </div>
     );
   };
